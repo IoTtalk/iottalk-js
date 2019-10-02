2f380d33910f79160373a70866dbd9b445268ae3
Iblis Lin <iblis@hs.ntnu.edu.tw>
2019-10-02 17:56:25 +0800

    update deps

--- examples/bulb/js/dai.js
+++ examples/bulb/js/dai.js
@@ -11,27 +11,28 @@ const dai = function (profile, ida) {
         profile.odf_list[i][0] = profile.odf_list[i][0].name;
     }
 
-    function on_data (odf_name, data) {
+    function on_data(odf_name, data) {
         df_func[odf_name](data);
     }
 
-    function on_signal (cmd, param) {
+    function on_signal(cmd, param) {
         console.log('[cmd]', cmd, param);
         return true;
     }
 
-    function init_callback (result) {
+    function init_callback(result) {
         console.log('register:', result);
         document.title = profile.d_name;
         ida.iot_app();
     }
 
-    dan2.register('http://140.113.123.218:9992', {
+    dan2.register('https://example.com/csm', {
         'id': app_id,
         'on_signal': on_signal,
         'on_data': on_data,
         'idf_list': profile['idf_list'],
         'odf_list': profile['odf_list'],
         'accept_protos': ['mqtt'],
+        'profile': profile,
     }, init_callback);
 };
