f644134f3f1643659662c026a348b2822f85aa33
Iblis Lin <iblis@hs.ntnu.edu.tw>
2021-04-13 15:32:42 +0800

    refine Dummy_Device example

--- examples/Dummy_Device/js/sa.js
+++ examples/Dummy_Device/js/sa.js
@@ -1,4 +1,4 @@
-/* global $:readonly iottalkjs:readonly */
+/* global iottalkjs:readonly */
 /* eslint camelcase: ["error", {"allow": ["Dummy_Sensor", "Dummy_Control"]}] */
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -18,9 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const option = {
     apiUrl: 'https://iottalk2.tw/csm',
     deviceModel: 'Dummy_Device',
-    deviceAddr: 'c96ca71c-9e48-2a23-2868-acb420a2f105',
     deviceName: 'MyDummyDevice',
-    persistentBinding: true,
     idfList: [[Dummy_Sensor, ['int']]],
     odfList: [Dummy_Control],
     pushInterval: 0,
