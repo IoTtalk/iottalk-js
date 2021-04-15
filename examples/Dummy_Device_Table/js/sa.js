f6b9f3471b3bcec4efd58f152c86b87fad8b0a0b
Iblis Lin <iblis@hs.ntnu.edu.tw>
2021-04-15 17:16:04 +0800

    js coding style

--- examples/Dummy_Device_Table/js/sa.js
+++ examples/Dummy_Device_Table/js/sa.js
@@ -2,46 +2,45 @@
 /* eslint camelcase: ["error", {"allow": ["Dummy_Sensor", "Dummy_Control"]}] */
 
 document.addEventListener('DOMContentLoaded', () => {
+  function createRow(id, number) {
+    const row = document.createElement('tr');
+    const block1 = document.createElement('td');
+    const block2 = document.createElement('td');
+    const value = document.createTextNode(number);
+    const timeStr = new Date().toLocaleTimeString(
+      'zh-TW',
+      {
+        hour12: false,
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        fractionalSecondDigits: 3,
+      },
+    );
 
-  function create_row(id, number){
-    var row = document.createElement('tr');
-    var block1 = document.createElement('td');
-    var block2 = document.createElement('td');
-    var value = document.createTextNode(number);
-    var _time = new Date().toLocaleTimeString(
-                  'en-US', 
-                  { 
-                    hour12: false,
-                    hour: "numeric",
-                    minute: "numeric",
-                    second: "numeric"
-                  }
-                );
-
-    var time = document.createTextNode(_time);
+    const time = document.createTextNode(timeStr);
 
     block1.appendChild(time);
     block2.appendChild(value);
     row.appendChild(block1);
     row.appendChild(block2);
 
-    var table = document.getElementById(id);
+    const table = document.getElementById(id);
     table.appendChild(row);
 
-    if (table.rows.length > 5){
-        table.deleteRow(1);
+    if (table.rows.length > 5) {
+      table.deleteRow(1);
     }
-
   }
 
   function Dummy_Sensor() {
     const number = Math.floor((1 + Math.random()) * 0x10000);
-    create_row('dummy-sensor', number);
+    createRow('dummy-sensor', number);
     return [number];
   }
 
   function Dummy_Control(data) {
-    create_row('dummy-control', data);
+    createRow('dummy-control', data);
   }
 
   const option = {
